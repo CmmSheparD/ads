f5b875ebe26aa2791e2fb1843e4e04847096b194
CmmSheparD <ustynow.gleb@gmail.com>
2022-10-08 00:16:30 +0300

    fix: Editorconfig is back! Also, problem solution interface should look like that.

---
+++ lab2-2022/src/snail.hh
@@ -0,0 +1,11 @@
+#ifndef SNAIL_HH
+#define SNAIL_HH
+
+struct Snail {
+	Snail(double x, double y) : x(x), y(y) {}
+
+	const double x;
+	const double y;
+};
+
+#endif	// SNAIL_HH
---
+++ lab2-2022/src/solve.hh
@@ -0,0 +1,10 @@
+#ifndef SOLVE_HH
+#define SOLVE_HH
+
+#include <vector>
+
+#include "snail.hh"
+
+double findClosestDistance(std::vector<Snail> snails);
+
+#endif	// SOLVE_HH

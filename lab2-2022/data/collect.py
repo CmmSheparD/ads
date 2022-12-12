83526a83fe087844b215127bb2684c164cba4c1c
CmmSheparD <ustynow.gleb@gmail.com>
2022-12-13 01:48:15 +0300

    feat: A fancier figure, with worst and best cases.

--- lab2-2022/data/collect.py
+++ lab2-2022/data/collect.py
@@ -17,11 +17,17 @@ if __name__ == '__main__':
                     res[int(matches[1])] = int(matches[2])
             for n, time in res.items():
                 results[n] = (*results[n], time) if n in results else (time,)
-    results = (
-        results.keys(),
-        [sum(times) / len(times) for times in results.values()]
-    )
-    plt.plot(*results)
+    worst = {}
+    average = {}
+    best = {}
+    for n, times in results.items():
+        worst[n] = max(times)
+        average[n] = sum(times) / len(times)
+        best[n] = min(times)
+    plt.plot(*zip(*best.items()), 'C3', label='Best')
+    plt.plot(*zip(*average.items()), 'C1', label='Average')
+    plt.plot(*zip(*worst.items()), 'b', label='Worst')
+    plt.legend()
     plt.xlabel('Number of entries')
-    plt.ylabel('Average calculation time, ms')
+    plt.ylabel('Calculation time, ms')
     plt.show()

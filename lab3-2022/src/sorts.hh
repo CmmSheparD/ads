797eb598c7ee23f7d56cccd458f78800ec77a0ce
CmmSheparD <ustynow.gleb@gmail.com>
2022-12-18 03:09:13 +0300

    feat: Selection sort.

--- lab3-2022/src/sorts.hh
+++ lab3-2022/src/sorts.hh
@@ -27,4 +27,26 @@ void insertionSort(RandIt first, RandIt last)
     insertionSort(first, last, std::less<typename RandIt::value_type>());
 }
 
+template<class RandIt, class Compare>
+void selectionSort(RandIt first, RandIt last, Compare cmp)
+{
+    if (first >= last)
+        throw std::range_error("First iterator equals or is behind the last");
+    RandIt shuttle;
+    RandIt most_extreme;
+    for (RandIt unsorted = first; unsorted != last; ++unsorted) {
+        most_extreme = unsorted;
+        for (shuttle = unsorted; shuttle != last; ++shuttle)
+            if (cmp(*shuttle, *most_extreme)) most_extreme = shuttle;
+
+        if (most_extreme != unsorted) std::swap(*most_extreme, *unsorted);
+    }
+}
+
+template<class RandIt>
+void selectionSort(RandIt first, RandIt last)
+{
+    selectionSort(first, last, std::less<typename RandIt::value_type>());
+}
+
 #endif	// SORTS_HH
